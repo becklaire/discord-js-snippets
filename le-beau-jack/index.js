1e7b45bf50c6f174aa252bfa0ce5653834e5745c
Claire Becker <contact.cbecker@gmail.com>
2023-10-21 23:59:11 +0200

    Tim is a savior

--- le-beau-jack/index.js
+++ le-beau-jack/index.js
@@ -1,34 +1,74 @@
 // Requirements
 const fs = require('node:fs'); // native module to read commands files
 const path = require('node:path'); // native module to help construct paths to access files and directories
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { REST, Routes, Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token, clientId, guildId} = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // Commands fetch
-client.commands = new Collection(); // mean to use retrieve commands for execution
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commands = [];
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	}
 }
 
-// LAUNCHER - LISTENER
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	console.log(interaction); //only catches slash interactions
-});
+// Set up for REST protocols
+const rest = new REST().setToken(token);
 
+// Allows synch
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands?.length} application (/) commands.`);
+		// put: refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+		console.log(`Successfully reloaded ${data?.length} application (/) commands.`);
+	} 
+	catch (error) {console.error(error);}
+})();
+
+// LAUNCHER
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
 // Discord logging
 client.login(token);
