96216f11eba342e70b82fbcea089becf0efb122c
Claire Becker <contact.cbecker@gmail.com>
2023-10-21 17:17:55 +0200

    cleaning

---
+++ le-beau-jack/index.js
@@ -0,0 +1,9 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Log in to Discord with your client's token
+client.login(token);
